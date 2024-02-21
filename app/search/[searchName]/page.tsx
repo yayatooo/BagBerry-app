561d84fc1c2935842917564253717dbb0f1fb687
Rahmat Hidayat <yayatbx@gmail.com>
2024-02-21 16:26:52 +0800

    test search data

--- app/search/[searchName]/page.tsx
+++ app/search/[searchName]/page.tsx
@@ -3,51 +3,51 @@ import React from "react";
 import searchParam from "@/app/service/searchParam";
 import { useQuery } from "react-query";
 import SearchProduct from "./components/SearchProduct";
+import { CardProductProps } from "@/app/type/CardProductProps";
+import Loading from "@/app/ui/Loading";
 
-// import SearchContainer from "./components/SearchContainer";
-
-export default function Page({ params }: { params: { searchName: string } }) {
-  //   const { data, isLoading } = useQuery(
-  //     ["search", params.searchName],
-  //     () => searchParam(params.searchName),
-  //     {
-  //       enabled: !!params.searchName,
-  //     }
-  //   );
-
-  //   console.log(data);
-
-  // Check if data is undefined or not an array
-  //   if (isLoading) return <div>Loading...</div>;
-  //   if (!data || !Array.isArray(data)) return null;
-
-  return (
-    <>
-      <h1 className="text-3xl font-semibold text-center py-36">
-        {params.searchName}
-      </h1>
-    </>
-  );
+interface DataSearchProps {
+  aaData: CardProductProps[];
 }
 
-/* <section className="container py-16">
-  <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      
-    {data === undefined ? (
-      <h1>loading</h1>
-    ) : (
-      <>
-        {data.map((item: any) => (
-          <SearchProduct
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            photo={item.photo}
-            currency={item.currency}
-            price={item.price}
-          />
-        ))}
-      </>
-    )}
-  </div>
-</section> */
+export default function Page({ params }: { params: { searchName: string } }) {
+  const { data, isLoading } = useQuery<DataSearchProps>(
+    ["search", params.searchName],
+    () => searchParam(params.searchName),
+    {
+      enabled: !!params.searchName,
+    }
+  );
+
+  console.log(data);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!data || !Array.isArray(data)) return null;
+
+  if (data.length === 0) {
+    return <Loading />;
+  } else {
+    return (
+      <section className="container py-16">
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {data.aaData.map((item: CardProductProps) => (
+            <SearchProduct
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              photo={item.photo}
+              currency={item.currency}
+              price={item.price}
+            />
+          ))}
+          {/* <h1 className="text-3xl py-48">{params.searchName}</h1> */}
+        </div>
+      </section>
+    );
+  }
+  //   return (
+  //     <div>
+  //       <p>{data.toString()}</p>
+  //     </div>
+  //   );
+}
